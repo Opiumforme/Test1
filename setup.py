62705262c3508826fd883e83a440ecefc52eafb3
unknown <tr4.0@rambler.ru>
2021-03-15 01:45:21 +0300

    init

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name="trello_client-basics-api-username", version="0.0.1", author="ВАШЕ ИМЯ", author_email="ВАШ ЭЛЕКТРОННЫЙ АДРЕС",
-    description="НЕБОЛЬШОЕ ОПИСАНИЕ ПРОЕКТА", long_description=long_description,
+    name="Opiumforme", version="0.0.1", author="Opiumforme", author_email="tr4.0@rambler.ru",
+    description="Начало изучения GIT", long_description=long_description,
     long_description_content_type="text/markdown", url="https://github.com/basics-api-username/trello_client",
     packages=setuptools.find_packages(),
     classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
